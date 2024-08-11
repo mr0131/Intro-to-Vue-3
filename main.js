1c933afc715d10a799404285e9bb2fdf42c7f803
MR <alfarhanmr3@gmail.com>
2024-08-11 14:07:33 +0200

    lägg till en titel, beskrivning och en bild

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp ({
+    data() {
+        return {
+            product: 'Socks',
+            description: "This is a paragraph with some text",
+            img: 'assets/images/socks_blue.jpg'
+        }
+    }
+})
